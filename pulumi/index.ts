00ea342dc7a8361826b34ce9495cd3ec1746f4d7
chadcumm <algeron@gmail.com>
2026-02-07 22:11:22 -0600

    fix: remove unused config variable in pulumi/index.ts
    The config variable was created but never used, causing a TypeScript strict mode build error.

--- pulumi/index.ts
+++ pulumi/index.ts
@@ -2,7 +2,6 @@ import * as pulumi from '@pulumi/pulumi'
 import * as aws from '@pulumi/aws'
 import * as awsx from '@pulumi/awsx'
 
-const config = new pulumi.Config()
 const region = aws.config.region || 'us-east-1'
 
 // Create S3 bucket for temporary PDFs
